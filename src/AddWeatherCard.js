39b6f0f80187c75579389c6f05cc5d07a984cd50
MirrorIce <12020876+MirrorIce@users.noreply.github.com>
2021-01-14 22:16:22 +0200

    Added localStorage in order to retain the selected city.

--- src/AddWeatherCard.js
+++ src/AddWeatherCard.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react'
 import CitySuggestionContainer from "./CitySuggestionContainer.js"
-
+//TODO Divide the functionality of toggleSuggestions into two: setCitySelection and hideSuggestionList. This way we ensure that we don't mix those two and have logic issues.
 function AddWeatherCard() {
     const [cityName, setCityName] = useState('');
     const [citySuggestions, setCitySuggestions] = useState([]);
@@ -49,10 +49,36 @@ function AddWeatherCard() {
             });
         }
     }
+
+    function addCity(event){
+        let cities = localStorage.getItem("weatherCities");
+        let arrayCities = JSON.parse(cities); 
+        if (isCitySelected == true)
+            if (arrayCities != undefined){
+                let cityAlreadyAdded = false;
+                arrayCities.forEach((value,index) => {
+                    if (value == cityName) cityAlreadyAdded = true;
+                })
+                if (cityAlreadyAdded == false){
+                    arrayCities.push(cityName);
+                    localStorage.setItem("weatherCities",JSON.stringify(arrayCities));
+                }
+                else{
+                    alert("This city was already added");
+                }
+            }
+            else{
+                localStorage.setItem("weatherCities",JSON.stringify([cityName]));
+            }
+        else{
+            alert("Please select a city from the list!");
+        }
+            
+    }
     return (
         <div className="addWeatherCard">
                 <h3>Add city</h3>
-                <button>+</button>
+                <button onClick = {addCity}>+</button>
                 <input onFocus={()=>{toggleSuggestions(true)}} onBlur = {()=>{toggleSuggestions(false)}} onChange={processCityNameInput} value={cityName} type='text' placeholder="City Name"></input>
                 <CitySuggestionContainer setCityInput={setCityName} setIsCitySelected = {setIsCitySelected} cities={citySuggestions}/>
             </div>
---
+++ src/WeatherCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function WeatherCard() {
+    return (
+        <div>
+            <h1>City Name</h1>
+            <p>Weather Pictogram</p>
+            <p>State</p>
+
+            <p>Min temp</p>
+            <p>Max temp</p>
+        </div>
+    )
+}
+
+export default WeatherCard
