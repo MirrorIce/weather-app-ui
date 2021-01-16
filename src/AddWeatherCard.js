661cee1f605c54075c8fa0263451dddfc07e2b6c
MirrorIce <12020876+MirrorIce@users.noreply.github.com>
2021-01-16 16:24:43 +0200

    Change the storage to retain more relevant information (eliminating redundant API calls ).

--- src/AddWeatherCard.js
+++ src/AddWeatherCard.js
@@ -15,7 +15,6 @@ function AddWeatherCard() {
         fetch('https://api.teleport.org/api/cities?search='+event.target.value)
             .then(response => response.json())
             .then((data) => {if (data!=null){
-                console.log(data);
                 setCitySuggestions((arr) =>{
                     //Setting the length to 0 effectively deletes the array
                     arr.length = 0;
@@ -44,27 +43,60 @@ function AddWeatherCard() {
             });
         }
     }
+    function populateCityDetails(cityName){
+        let cityDetails = {
+            name:cityName,
+            lat:0,
+            long:0,
+            geoid:0,
+            tzoffset:0
+        }
+        return new Promise(function(resolve){
+            //Fetch the city GeoID
+            fetch('https://api.teleport.org/api/cities?search='+cityName)
+                .then(response => response.json())
+                .then((data) => {if (data!=null){
+                     let cityURL = data['_embedded']['city:search-results'][0]['_links']['city:item']['href'];
+                     let cityGeoId = cityURL.split('/')[5];
+                     cityDetails.geoid = cityGeoId;
+                    fetch('https://api.teleport.org/api/cities/'+cityGeoId)
+                     .then(response => response.json())
+                      .then((data) => {
+                          cityDetails.lat = data.location.latlon.latitude;
+                          cityDetails.lat = data.location.latlon.longitude;
+                          resolve(cityDetails);
+                })
 
+            } 
+        });
+        });
+        
+    }
     function addCity(event){
         let cities = localStorage.getItem("weatherCities");
         let arrayCities = JSON.parse(cities); 
-        if (isCitySelected == true)
-            if (arrayCities != undefined){
-                let cityAlreadyAdded = false;
-                arrayCities.forEach((value,index) => {
-                    if (value == cityName) cityAlreadyAdded = true;
-                })
-                if (cityAlreadyAdded == false){
-                    arrayCities.push(cityName);
-                    localStorage.setItem("weatherCities",JSON.stringify(arrayCities));
+        if (isCitySelected == true){
+            let detailedCity = null;
+            populateCityDetails(cityName)
+            .then((detailedCity) =>{
+                if (arrayCities != undefined){
+                    let cityAlreadyAdded = false;
+                    arrayCities.forEach((value,index) => {
+                        if (value.name == cityName) cityAlreadyAdded = true;
+                    })
+                    if (cityAlreadyAdded == false){
+                        arrayCities.push(detailedCity);
+                        localStorage.setItem("weatherCities",JSON.stringify(arrayCities));
+                    }
+                    else{
+                        alert("This city was already added");
+                    }
                 }
                 else{
-                    alert("This city was already added");
+                    localStorage.setItem("weatherCities",JSON.stringify([detailedCity]));
                 }
-            }
-            else{
-                localStorage.setItem("weatherCities",JSON.stringify([cityName]));
-            }
+            })
+        }
         else{
             alert("Please select a city from the list!");
         }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
 import AddWeatherCard from './AddWeatherCard.js'
-
+import WeatherCard from './WeatherCard.js'
 function App() {
-
+  //TODO Think about a proper data structure in order to store temperature data. It must include a method to keep several days of forecast.
   let cities = JSON.parse(localStorage.getItem('weatherCities'));
   
   return (
@@ -11,19 +12,20 @@ function App() {
       <h1>Weathery</h1>
       <h2>Your friendly weather app</h2>
       <AddWeatherCard></AddWeatherCard>
-      {cities.map((city,cityIndex)=>{
-        fetch('https://api.teleport.org/api/cities?search='+city)
-        .then(response => response.json())
-        .then((data) => {if (data!=null){
-            let cityURL = data['_embedded']['city:search-results'][0]['_links']['city:item']['href'];
-            let cityGeoId = cityURL.split('/')[5];
-            console.log(cityGeoId);
-
-            } 
-          });
+      <p style={{color:"red"}}>NOTE: THERE IS NOT TIMEZONE IMPLEMENTATION ATM SO THE DISPLAYED TIME MAY BE WRONG (100%)</p>
+      {cities!=null?cities.map((city,cityIndex)=>{
+              fetch('http://www.7timer.info/bin/civil.php?lon='+city.long+'&lat='+city.lat+'&ac=0&unit=metric&output=json&tzshift=0')
+              .then(response => response.json())
+              .then((data) => {
+                console.log("XX");
+                return(
+                  <WeatherCard />
+                )
+              })
         }
       
-      )}
+      ):null
+      }
 
 
 
